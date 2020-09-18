9bedb1a3e8356c253b1ea4798dbd7d13ba96f511
LennWi <70192362+LennWi@users.noreply.github.com>
2020-09-18 12:37:51 +0200

    Projektdateien hinzufügen.

---
+++ Uebung2_HSP_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uebung2_HSP_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            double zahl = 1.0;
+            
+            for (int i = 0; i < 12; i++)
+            {
+                zahl = zahl - 0.1;
+                
+                if (Math.Round(zahl, 1) == 0)
+                {
+                    Console.WriteLine(Math.Round(zahl, 1) + " <- Da ist die Null!");
+                }
+                else
+                {
+                    Console.WriteLine(Math.Round(zahl, 1));
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
